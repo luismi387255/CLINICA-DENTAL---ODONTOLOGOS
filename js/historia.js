6736a23ed7645ba046f164da3d5b5f55d37e60ca
luismi387255 <106945705+luismi387255@users.noreply.github.com>
2022-06-06 00:44:23 -0500

    Add files via upload

---
+++ js/historia.js
@@ -0,0 +1,10 @@
+function text(x){
+    if (x==1) document.getElementById("Cual").style.display="block";
+    else document.getElementById("Cual").style.display="none";
+    
+    }
+    function desactivarcaja()
+    {
+    document.getElementById("Cual").disable=true
+    }
+    
